684595123cfbdd21ba6e61d16a10c30410e62212
Mathias Aschhoff <web@little-devil.de>
2019-12-08 11:29:25 +0100

    Add files via upload
    experimental without id for temp sensor.

--- RecordReceiveCallback.py
+++ RecordReceiveCallback.py
@@ -17,21 +17,49 @@ def auswertung(code):		# was passiert mit code
 # Empfang json Format
 
 	protocol_weather_list = {'alecto_ws1700','teknihall'}
-	protocol_contact_list = {'GS-iwds07'}
-	item_list = {38586: 'Kuechenfenster', 153:'Temp1_String', 85: 'Temp2_String'}
+	protocol_contact_list = {'GS-iwds07','arctech_screen_old'}
+	protocol_bell_list = {'ev1527'}
+	item_list = {17479:'Wohnzimmerfenster_rechts', 
+				10198:'Wohnzimmerfenster_links',	
+				23302:'Kellertuer',	
+				41434:'Wintergartenfenster', 
+				474:'Schlafzimmerfenster', 
+				38586: 'Kuechenfenster', 
+				139:'Temp1_String', 
+				180: 'Temp2_String', 
+				800581:'Bell_String',
+				30164: 'Badezimmerfenster',
+				26324: 'Tuer',
+				2:'Bewegung_x',
+				15:'Bewegung_y'}
 	
 
 
 	#if code["protocol"] == "alecto_ws1700" or code["protocol"] == "teknihall":
-	if code["protocol"] in protocol_weather_list:
+	if code["protocol"] == "teknihall":
 		id=code["message"]["id"]
 		temp=code["message"]["temperature"]
 		humidity=code["message"]["humidity"]
-		if id in item_list:
-			sendToOpenHAB(item_list[id],str(round(temp,9)))
-			sendToOpenHAB(item_list[id]+'_Humidity',str(round(humidity,9)))
+		sendToOpenHAB("Temp1_String",str(temp))
+		sendToOpenHAB("Temp1_String_Humidity",str(round(humidity,9)))
+
+	if code["protocol"] == "alecto_ws1700":
+		id=code["message"]["id"]
+		temp=code["message"]["temperature"]
+		humidity=code["message"]["humidity"]
+		
+		logging.debug('Alecto INFO:\n ID:'+str(id)+'\n HUM: '+str(humidity)+'\n TEMP: '+str(temp))
+
+		sendToOpenHAB('Temp2_String',str(round(temp,9)))
+		sendToOpenHAB('Temp2_String_Humidity',str(round(humidity,9)))
+	
 	if code["protocol"] in protocol_contact_list:
 		id=code["message"]["unit"]
+		data=code["message"]["state"]
+		if id in item_list:
+			sendToOpenHAB(item_list[id],data)
+	if code["protocol"] in protocol_bell_list:
+		id=code["message"]["unitcode"]
 		data=code["message"]["state"]
 		if id in item_list:
 			sendToOpenHAB(item_list[id],data)
@@ -40,7 +68,7 @@ def auswertung(code):		# was passiert mit code
 def sendToOpenHAB(item,data):
 	logging.info("SEND TO OPENHAB - ITEM: "+item+" DATA: "+data)
 	# OpenHAB API aufrufen
-	url = "http://10.10.10.4:8080/rest/items/"+item
+	url = "http://192.168.0.4:8080/rest/items/"+item
 	#data = str(round(temp,9))
 	#data_json = json.dumps(data) # bei json data={"jsondata"} und als response data=data_json
 	headers = {'Content-type': 'text/plain'}
@@ -69,6 +97,6 @@ def run():
 if __name__ == '__main__':
 	try:
 		run()
-	except:
-		e=sys.exec_info()[0]
-		logging.warn("Exception: " % e)
+	except Exception as e:
+		logging.warn("Exception: "+repr(e))
+		exit()
